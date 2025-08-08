dd7d84e048580e88bd97a529e1a72dd06046b897
Akila Shashimantha <akilashashimantha84@gmail.com>
2025-08-08 20:07:40 +0530

    change bgcolor when change the screen sizes

--- lib/widgets/summary_widget.dart
+++ lib/widgets/summary_widget.dart
@@ -1,4 +1,5 @@
 import 'package:fitness_tracker/constants/colors.dart';
+import 'package:fitness_tracker/utils/responsive.dart';
 import 'package:fitness_tracker/widgets/pie_chart.dart';
 import 'package:fitness_tracker/widgets/shedule_widget.dart';
 import 'package:fitness_tracker/widgets/summary_details.dart';
@@ -14,24 +15,68 @@ class Summary extends StatefulWidget {
 class _SummaryState extends State<Summary> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-       children: [
-        PieChartCard(),
-            Text(
-              "Summery", style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: greyColor,
+    final isDesktop = Responsive.isDesktop(context);
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: !isDesktop ? cardBackgroundColor : null,
+      ),
+      child: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Pie Chart Section
+              Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: cardBackgroundColor,
+                  ),
+                  height: 200,
+                  child: PieChartCard(),
+                ),
               ),
-            ),
-            SizedBox(height: 16,),
-            SummeryDetailes(),
-            SizedBox(height: 16,),
-            SheduleWidget(),
-          ],
-        
+              SizedBox(height: 20),
+              
+              // Summary Title
+              Text(
+                "Summary", 
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: greyColor,
+                ),
+              ),
+              SizedBox(height: 16),
+              
+              // Summary Details Section
+              Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: cardBackgroundColor,
+                  ),
+                  padding: EdgeInsets.all(8),
+                  child: SummeryDetailes(),
+                ),
+              ),
+              SizedBox(height: 20),
+              
+              // Schedule Section
+              Card(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: cardBackgroundColor,
+                  ),
+                  padding: EdgeInsets.all(8),
+                  child: SheduleWidget(),
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
     );
   }
