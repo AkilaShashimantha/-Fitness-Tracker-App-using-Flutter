a94927184619af63e3f8cffbb986017a1b11eb39
Akila Shashimantha <akilashashimantha84@gmail.com>
2025-08-08 20:04:34 +0530

    Make Components visible screen sizes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:fitness_tracker/utils/responsive.dart';
 import 'package:fitness_tracker/widgets/dashboard.dart';
 import 'package:fitness_tracker/widgets/side_menu.dart';
+import 'package:fitness_tracker/widgets/summary_details.dart';
 import 'package:fitness_tracker/widgets/summary_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +10,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+final bool isDesktop = Responsive.isDesktop(context);
+
     return Scaffold(
+
+drawer: !isDesktop ? SizedBox(
+  width: MediaQuery.of(context).size.width*0.8,
+  child: SideMenu(),
+): null,
+
+endDrawer: !isDesktop ? SizedBox(
+  width: MediaQuery.of(context).size.width*0.8,
+  child: Summary(),
+): null ,
+
       body: SafeArea(child: Row(
         children: [
          
          //Side Menu
-          Expanded(
+         if (isDesktop) Expanded(
             flex: 2,
             child: SizedBox(
              child: SideMenu(),
@@ -29,7 +45,7 @@ class HomePage extends StatelessWidget {
           ),
 
           //Summary
-          Expanded(
+         if (isDesktop) Expanded(
             flex: 4,
             child: SizedBox(
               child: Summary(),
